14d825df08150e05aa8abc2d213a163c5f7ed060
think-nix <81829935+nicosan001@users.noreply.github.com>
2023-02-01 21:06:02 -0700

    apply-system script

--- apply-system.sh
+++ apply-system.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 pushd ~/.dotfiles
+cp system/hardware-configuration.nix system/hardware-configuration.nix.bak
+cp /etc/nixos/hardware-configuration.nix system/hardware-configuration.nix
 sudo nixos-rebuild switch --flake .#
 popd
