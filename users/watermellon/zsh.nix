0a2d56c32b1abc6d4d003f1d5b463c2ebc2762ed
think-nix <81829935+nicosan001@users.noreply.github.com>
2023-02-04 16:32:17 -0700

    zsh config

--- users/watermellon/zsh.nix
+++ users/watermellon/zsh.nix
@@ -4,6 +4,10 @@
   programs.zsh.enable = true;
 
  programs.zsh = {
+   shellAliases = {
+     l = "exa -lah --icons";
+     ls = "exa -lah --icons";
+   };
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" ];
