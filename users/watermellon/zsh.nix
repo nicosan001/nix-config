aa9ea718edef9fb7630af68380113582775e2e11
think-nix <81829935+nicosan001@users.noreply.github.com>
2023-02-04 18:51:48 -0700

    messing with zsh config

--- users/watermellon/zsh.nix
+++ users/watermellon/zsh.nix
@@ -1,30 +1,44 @@
 { libs, config, pkgs, ... }:
 {
+
   home.packages = with pkgs; [
     meslo-lgs-nf
+    zsh-powerlevel10k
   ];
+
   # change shell to zsh
   programs.zsh.enable = true;
 
  programs.zsh = {
+
+   enableAutosuggestions = true;
+   
+
    shellAliases = {
      l = "exa -lah --icons";
      ls = "exa -lah --icons";
    };
-   initExtra = ''
-   [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-   '';
-    plugins = with pkgs; [
-    {
-      file = "powerlevel10k.zsh-theme";
-      name = "powerlevel10k";
-      src = "${zsh-powerlevel10k}/share/zsh-powerlevel10k";
-    }
-    {
-      name = "powerlevel10k-config";
-      src = lib.cleanSource ./p10k-config;
-      file = "p10k.zsh";
-    }
+
+   plugins = with pkgs; [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = lib.cleanSource ./p10k-config;
+        file = "p10k.zsh";
+      }
   ];
+   
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "robbyrussell";
+    };
   };
+
+# programs.zsh.promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+
 }
