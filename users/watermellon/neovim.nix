e33772d3426d4d47475add37e1eca3bca0dd780f
think-nix <81829935+nicosan001@users.noreply.github.com>
2023-02-04 20:49:21 -0700

    added sway-variables

--- users/watermellon/neovim.nix
+++ users/watermellon/neovim.nix
@@ -1,15 +1,55 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
+
+let
+  install_lsp = pkgs.writeShellScriptBin "install_lsp" ''
+      #!/bin/bash 
+    if [ ! -d ~/.npm-global ]; then  
+             mkdir ~/.npm-global
+             npm set prefix ~/.npm-global
+      else 
+             npm set prefix ~/.npm-global
+    fi
+    npm i -g npm vscode-langservers-extracted vscode-langservers-extracted typescript typescript-language-server bash-language-server
+  '';
+in
 {
-  home.file = {
-    ".config/nvim/init.vim".text = ''
-      set number relativenumber
-      set nu rnu
-      set clipboard^=unnamed,unnamedplus
-      set smartindent
-      set tabstop=2
-      set expandtab
-      set shiftwidth=2
-      syntax on
-    '';
+  programs = {
+    neovim = {
+      enable = true;
+      withPython3 = true;
+      withNodeJs = true;
+      extraPackages = [
+      ];
+      #-- Plugins --#
+      plugins = with pkgs.vimPlugins;[ ];
+      #-- --#
+    };
   };
+  home = {
+    packages = with pkgs; [
+      #-- LSP --#
+      install_lsp
+      rnix-lsp
+      sumneko-lua-language-server
+      gopls
+      pyright
+      zk
+      rust-analyzer
+      clang-tools
+      #-- tree-sitter --#
+      tree-sitter
+      #-- format --#
+      stylua
+      black
+      nixpkgs-fmt
+      rustfmt
+      beautysh
+      nodePackages.prettier
+      #-- Debug --#
+      lldb
+    ];
+  };
+
+  home.file.".config/nvim/init.lua".source = ./nvim/init.lua;
+  home.file.".config/nvim/lua".source = ./nvim/lua;
 }
